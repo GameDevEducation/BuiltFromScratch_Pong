623ae957cd7d076dc651053f00f7e05e34d2d513
Iain McManus <imcmanus@injaia.com>
2021-07-11 11:49:04 +1000

    Add in ball and paddle movement

---
+++ Assets/Scripts/Ball/BallController.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallController : MonoBehaviour
+{
+    public enum ELaunchDirection
+    {
+        Random,
+        TowardsPlayer1,
+        TowardsPlayer2
+    }
+
+    public enum ELastTouchedBy
+    {
+        NoOne,
+        Player1,
+        Player2
+    }
+
+    [SerializeField] ELaunchDirection LaunchDirection = ELaunchDirection.Random;
+    [SerializeField] float InitialSpeed = 30f;
+    [SerializeField] float MinimumSpeed = 20f;
+    [SerializeField] float MaximumSpeed = 50f;
+    [SerializeField] float MaxLaunchAngle = 30f;
+    [SerializeField] float PaddleVelocityWeight = 1f;
+
+    [SerializeField] bool AutoLaunch = true;
+
+    Rigidbody BallRB;
+    ELastTouchedBy LastTouchedBy = ELastTouchedBy.NoOne;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BallRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 CurrentVelocity;
+    void FixedUpdate()
+    {
+        // perform auto launch?
+        if (AutoLaunch)
+        {
+            AutoLaunch = false;
+            Launch();
+        }
+
+        // we're sleeping - do nothing
+        if (BallRB.IsSleeping())
+            return;
+
+        // clamp the ball velocity if needed
+        if (BallRB.velocity.magnitude < MinimumSpeed || BallRB.velocity.magnitude > MaximumSpeed)
+            BallRB.velocity = BallRB.velocity.normalized * Mathf.Clamp(BallRB.velocity.magnitude, MinimumSpeed, MaximumSpeed);
+
+        CurrentVelocity = BallRB.velocity;
+    }
+
+    public void Launch()
+    {
+        // pick the launch vector
+        Vector3 launchVector = Vector3.zero;
+        if (LaunchDirection == ELaunchDirection.Random)
+            launchVector = Random.Range(0, 2) == 0 ? Vector3.left : Vector3.right;
+        else if (LaunchDirection == ELaunchDirection.TowardsPlayer1)
+            launchVector = Vector3.left;
+        else
+            launchVector = Vector3.right;
+
+        // pick a launch angle
+        launchVector = Quaternion.Euler(0f, Random.Range(-MaxLaunchAngle, MaxLaunchAngle), 0f) * launchVector;
+
+        // launch the ball
+        BallRB.velocity = launchVector * InitialSpeed;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player1Paddle"))
+        {
+            LastTouchedBy = ELastTouchedBy.Player1;
+
+            OnHitPaddle(other);
+        }
+        else if (other.gameObject.CompareTag("Player2Paddle"))
+        {
+            LastTouchedBy = ELastTouchedBy.Player2;
+
+            OnHitPaddle(other);
+        }
+        else if (other.gameObject.CompareTag("Wall"))
+        {
+            OnHitWall(other);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player1Goal"))
+        {
+            Debug.Log("Player 1");
+        }
+        else if (other.CompareTag("Player2Goal"))
+        {
+            Debug.Log("Player 2");
+        }
+    }
+
+    void OnHitPaddle(Collision other)
+    {
+        // TODO - sound effect
+
+        // reflect the velocity
+        Vector3 reflectedVelocity = Vector3.Reflect(CurrentVelocity, other.GetContact(0).normal);        
+
+        // blend with the paddle velocity
+        Vector3 paddleVelocity = other.gameObject.GetComponent<PaddleController>().SimulatedVelocity;
+        BallRB.velocity = reflectedVelocity + paddleVelocity * PaddleVelocityWeight;
+    }
+
+    void OnHitWall(Collision other)
+    {
+        // TODO - sound effect
+
+        // reflect the velocity
+        BallRB.velocity = Vector3.Reflect(CurrentVelocity, other.GetContact(0).normal);        
+    }
+}
---
+++ Assets/Scripts/Paddle/PaddleController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PlayerInput))]
+public class PaddleController : MonoBehaviour
+{
+    public enum EMovementMode
+    {
+        Inertialess,
+        Inertia
+    }
+
+    [SerializeField] EMovementMode Movement = EMovementMode.Inertialess;
+    [SerializeField] float Speed = 1f;
+    [SerializeField] float MaxMovement = 3.5f;
+    [SerializeField] float SpeedSlewRate = 1f;
+
+    Vector3 OriginPoint;
+
+    Rigidbody PaddleRB;
+
+    public Vector3 SimulatedVelocity
+    {
+        get
+        {
+            // at the edge - no movement
+            if (Mathf.Abs(transform.position.y) >= MaxMovement)
+                return Vector3.zero;
+
+            if (Movement == EMovementMode.Inertia)
+                return transform.forward * Speed * Input_Move;
+
+            if (Movement == EMovementMode.Inertia)
+                return transform.forward * Speed * Input_Move_WithInertia;
+
+            return Vector3.zero;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PaddleRB = GetComponent<Rigidbody>();
+
+        OriginPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    float Input_Move_WithInertia;
+    void Update()
+    {
+        if (Mathf.Sign(Input_Move) != Mathf.Sign(Input_Move_WithInertia))
+            Input_Move_WithInertia = 0f;
+            
+        Input_Move_WithInertia = Mathf.MoveTowards(Input_Move_WithInertia, Input_Move, SpeedSlewRate * Time.deltaTime);
+    }
+
+    float Input_Move;
+    void OnMove(InputValue moveInput)
+    {
+        Input_Move = moveInput.Get<Vector2>().y;
+    }
+
+    void FixedUpdate()
+    {
+        if (Movement == EMovementMode.Inertialess)
+        {
+            // calculate the clamped new position
+            float newZPosition = transform.position.z + (Input_Move * Speed * Time.deltaTime);
+            newZPosition = Mathf.Clamp(newZPosition, -MaxMovement, MaxMovement);
+
+            // update the position
+            PaddleRB.MovePosition(OriginPoint + new Vector3(0, 0, newZPosition));
+        }
+        else if (Movement == EMovementMode.Inertia)
+        {
+            // calculate the clamped new position
+            float newZPosition = transform.position.z + (Input_Move_WithInertia * Speed * Time.deltaTime);
+            newZPosition = Mathf.Clamp(newZPosition, -MaxMovement, MaxMovement);
+
+            // update the position
+            PaddleRB.MovePosition(OriginPoint + new Vector3(0, 0, newZPosition));
+        }
+    }
+}
